88fe0dbfb671033e7953ee0dbfd96b010b19785a
numanmushtaq01 <naveenmushtaq3@gmail.com>
2026-05-11 18:28:12 +0530

    pipa: Clover Bringup

--- clover_pipa.mk
+++ clover_pipa.mk
@@ -9,15 +9,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/clover/config/common_full_tablet_wifionly.mk)
 
 # Inherit from pipa device
 $(call inherit-product, device/xiaomi/pipa/device.mk)
 
-# Inherit keys
-$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+#clover flags
+CLOVER_BUILDTYPE := OFFICIAL
+CLOVER_MAINTAINER := NumaanMushtaq
 
-PRODUCT_NAME := lineage_pipa
+PRODUCT_NAME := clover_pipa
 PRODUCT_DEVICE := pipa
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
