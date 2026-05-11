d9c5d6b1d27a020b43fa3d1745e5644a026f4aac
numanmushtaq01 <naveenmushtaq3@gmail.com>
2026-05-11 17:29:44 +0530

    pipa: Update props from pipa OS2.0.14.0.UMZCNXM

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -29,4 +29,4 @@ TARGET_SUPPORTS_QUICK_TAP := false
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=Xiaomi/pipa_global/pipa:13/RKQ1.211001.001/V816.0.7.0.UMZMIXM:user/release-keys
+    BuildFingerprint=Xiaomi/pipa_pad_cn/pipa:14/UKQ1.240624.001/OS2.0.14.0.UMZCNXM:user/release-keys  \
